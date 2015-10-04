2f27379adc07b3e67c8de99f8e434acdc324dd09
pheei <phe@gwu.edu>
2015-10-04 17:08:30 -0400

    update
    update

--- ZoneApp/app/src/main/java/com/zone/zoneapp/LoginActivity.java
+++ ZoneApp/app/src/main/java/com/zone/zoneapp/LoginActivity.java
@@ -6,8 +6,13 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button mUsernameButton;
+    Button mPasswordButton;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +30,9 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+
+
+
+
 
 }

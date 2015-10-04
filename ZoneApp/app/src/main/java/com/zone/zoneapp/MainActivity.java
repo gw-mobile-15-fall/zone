fd1a07346b001d2bee6d1812d3d6fd6ba52bf174
pheei <phe@gwu.edu>
2015-10-04 18:34:44 -0400

    create request activity
    update

--- ZoneApp/app/src/main/java/com/zone/zoneapp/MainActivity.java
+++ ZoneApp/app/src/main/java/com/zone/zoneapp/MainActivity.java
@@ -1,13 +1,18 @@
 package com.zone.zoneapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     String mUserName;
     TextView mWelcomeTextView;
+
+    Button mCreateRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,16 @@ public class MainActivity extends AppCompatActivity {
 
         mWelcomeTextView = (TextView)findViewById(R.id.welcome_user_textView);
         mWelcomeTextView.setText("Welcome " + mUserName + " !");
+
+        mCreateRequest = (Button)findViewById(R.id.create_request_Button);
+        mCreateRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, CreateRequestActivity.class);
+                i.putExtra(LoginActivity.EXTRA_USERNAME, mUserName);
+                startActivity(i);
+            }
+        });
     }
 
 

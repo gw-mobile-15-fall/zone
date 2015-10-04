0b51028645857b0b5e6da209e38ba1aa1c784d66
pheei <phe@gwu.edu>
2015-10-04 16:56:03 -0400

    update
    update

---
+++ ZoneApp/app/src/main/java/com/zone/zoneapp/UserAccount.java
@@ -0,0 +1,35 @@
+package com.zone.zoneapp;
+
+/**
+ * Created by hpishepei on 10/4/15.
+ */
+public class UserAccount {
+
+    private String mUserName;
+    private String mPassword;
+    private String mEmail;
+
+    public String getEmail() {
+        return mEmail;
+    }
+
+    public void setEmail(String mEmail) {
+        this.mEmail = mEmail;
+    }
+
+    public String getPassword() {
+        return mPassword;
+    }
+
+    public void setPassword(String mPassword) {
+        this.mPassword = mPassword;
+    }
+
+    public String getUserName() {
+        return mUserName;
+    }
+
+    public void setUserName(String mUserName) {
+        this.mUserName = mUserName;
+    }
+}
